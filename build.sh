c135f124a3809c56e1b11ab6f64a85c11edef982
jvtrudel <jvtrudel@gmx.com>
2016-02-24 15:45:56 -0500

    .

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-sass sass/main.sass public/css/style.css
+formats=('portable' 'desktop' 'tablet' 'hdscreen')
+
+for i in $formats; do
+  sass sass/$i.sass public/css/$i.css
+done
 
 mustache content/_accueil.yml template/accueil.mustache > public/index.html

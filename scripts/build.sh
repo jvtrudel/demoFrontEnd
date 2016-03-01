f9925ed18b8cce0c835406ec796cfde07291fc79
jvtrudel <jvtrudel@gmx.com>
2016-02-29 22:03:23 -0500

    .

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,9 +7,10 @@ formats=('style' 'phone' 'desktop' 'tablet' 'hdscreen')
 PROJECT_ROOT='projects'
 LAB_CORE_ROOT="core"
 
-tmp='.tmp_sass'
-echo $PROJECT_ROOT/$projName/sass/* 
 
+tmp='.tmp_sass'
+rm -rf $tmp
+mkdir $tmp
 
 cp -rf $LAB_CORE_ROOT/sass/* $tmp
 cp -rf $PROJECT_ROOT/$projName/sass/* $tmp
@@ -19,10 +20,10 @@ for i in ${formats[@]}; do
 done
 
 
-#for i in ${files[@]}; do
-#  cat $LAB_CORE_ROOT/templates/header.mustache > temp.mustache
-#  cat $LAB_CORE_ROOT/templates/$i.mustache >> temp.mustache
-#  cat template/footer.mustache >> temp.mustache
-#  mustache content/$i.yml temp.mustache > public/$i.html
-#  rm  -rf temp.mustache
-#done
+for i in ${files[@]}; do
+  cat $LAB_CORE_ROOT/templates/header.mustache > temp.mustache
+  cat $PROJECT_ROOT/$projName/templates/$i.mustache >> temp.mustache
+  cat $LAB_CORE_ROOT/templates/footer.mustache >> temp.mustache
+  mustache $PROJECT_ROOT/$projName/content/$i.yml temp.mustache > public/$i.html
+  rm  -rf temp.mustache
+done
